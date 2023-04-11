45402b1cb7ea3135d5adceb687c617cff46688e5
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-04-11 16:17:47 -0400

    Streams #2

--- 16-streams.js
+++ 16-streams.js
@@ -1,1 +1,16 @@
-const { readFileStream } = require('fs')
+// Streams - read or write sequentially
+
+// Read the file in chunks (64KB)
+
+const { createReadStream } = require('fs');
+
+const stream = createReadStream('./content/big.txt', { highWaterMark: 90000, encoding: 'utf-8'})
+
+// highWaterMark - control size
+  // const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 }) // default 64KB
+  // const stream = createReadStream('./content/big.txt', { encoding: 'utf8' })
+// last buffer - remainder
+
+stream.on('data', (result) => {
+  console.log(result)
+})
--- app.js
+++ app.js
@@ -4,8 +4,15 @@
 
 const { createReadStream } = require('fs');
 
-const stream = createReadStream('./content/big.txt')
+const stream = createReadStream('./content/big.txt', { highWaterMark: 80000, encoding: 'utf-8'})
+
+// highWaterMark - control size
+  // const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 }) // default 64KB
+  // const stream = createReadStream('./content/big.txt', { encoding: 'utf8' })
+// last buffer - remainder
 
 stream.on('data', (result) => {
   console.log(result)
 })
+
+stream.on('error', (err) => console.log(err))
