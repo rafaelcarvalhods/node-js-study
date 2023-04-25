e533035be93df0af6067c1ed8c979c5f1927f0f8
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-04-25 16:27:21 -0400

    Read html file

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,9 +1,26 @@
 const http = require('http');
+const { readFileSync } = require('fs')
+
+const homePage = readFileSync("./index.html")
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'content-type': 'text/html'});
-  res.write('<h1>Home Page</h1>')
-  res.end()
+
+  const url = req.url
+
+  if (url === "/") {
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.write(homePage)
+    res.end()
+  } else if (url === "/about") {
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.write('<h1>About Page</h1>')
+    res.end()  
+  } else {
+    res.writeHead(404, {'content-type': 'text/html'});
+    res.write('<h1>Page Not Found</h1>')
+    res.end()
+  }
+  
 })
 
 server.listen(5000)
