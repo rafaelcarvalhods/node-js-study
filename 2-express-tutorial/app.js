c8845c4be1c24cc90a102edea84fe7c9221291d4
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-04-29 11:12:29 -0400

    Express basics

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,41 +1,18 @@
-const http = require('http');
-const { readFileSync } = require('fs')
+const express = require('express')
+const app = express()
 
-const homePage = readFileSync("./navbar-app/index.html")
-const homeLogic = readFileSync("./navbar-app/browser-app.js")
-const homeStyle = readFileSync("./navbar-app/styles.css")
-const homeImage = readFileSync("./navbar-app/logo.svg")
-
-const server = http.createServer((req, res) => {
-
-  const url = req.url
-
-  if (url === "/") {
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.write(homePage)
-    res.end()
-  } else if (url === "/about") {
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.write('<h1>About Page</h1>')
-    res.end()
-  } else if (url === "/browser-app.js") {
-    res.writeHead(200, {'content-type': 'text/javascript'});
-    res.write(homeLogic)
-    res.end()
-  } else if (url === "/styles.css") {
-    res.writeHead(200, {'content-type': 'text/css'});
-    res.write(homeStyle)
-    res.end()
-  } else if (url === "/logo.svg") {
-    res.writeHead(200, {'content-type': 'image/svg+xml'});
-    res.write(homeImage)
-    res.end()
-  } else {
-    res.writeHead(404, {'content-type': 'text/html'});
-    res.write('<h1>Page Not Found</h1>')
-    res.end()
-  }
-  
+app.get('/', (req, res) => {
+  res.status(200).send('Home Page')
 })
 
-server.listen(5000)
+app.get('/about', (req, res) => {
+  res.status(200).send('About Page')
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('Page Not Found')
+})
+
+app.listen(5000, () => {
+  console.log('The server is on the port 5000...')
+})
---
+++ 2-express-tutorial/final/1-http-basics.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const { readFileSync } = require('fs')
+
+// Be careful with file's path
+const homePage = readFileSync('index.html')
+
+const server = http.createServer((req, res) => {
+  const url = req.url
+
+  if(url === '/') {
+    res.writeHead(200, { 'content-type' : 'text/html'})
+    res.write(homePage)
+    res.end()
+  }
+  else if(url === '/about') {
+    res.writeHead(200, {'content-type' : 'text/html'})
+    res.write('<h1>About Page</h1>')
+    res.end()
+  }
+  else {
+    res.writeHead(404, { 'content-type':'text/html'})
+    res.write('<h1>Error! Page not found!</h1>')
+    res.end()
+  }
+})
+
+server.listen(5000)
